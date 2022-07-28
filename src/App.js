a8e775136519f248a74401ce1251a7a0cbc25b2e
MNishita <mnishita1029@gmail.com>
2022-07-28 19:22:50 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.css';
+import {FaBars} from "react-icons/fa";
+
+function Header() {
+    return (<div>
+        <FaBars className="icon"/>
+        Target Pick
+    </div>)
+}
+export default Header
