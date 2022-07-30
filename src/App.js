d1c94763a8e60f717c9ec99d73d4065ff9375ea2
MNishita <mnishita1029@gmail.com>
2022-07-30 10:34:20 +0530

    final header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import Footer from './components/bottombar';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import './index.css';
 import {FaBars} from "react-icons/fa";
 
 function Header() {
-    return (<div>
+    return (<div className="header">
         <FaBars className="icon"/>
         Target Pick
     </div>)
