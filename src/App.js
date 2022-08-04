273dd5f900f8c97e24ecde48bb430cfaf9e02bca
MNishita <mnishita1029@gmail.com>
2022-08-04 20:45:59 +0530

    Main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/header';
 import Footer from './components/bottombar';
+import Content from './pages/main/content';
+import Order from './pages/order/order-details';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Content/>
       <Footer/>
     </div>
   );
---
+++ src/pages/main/content.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./content.css";
+import MyImage from "./Heart-Target.webp";
+
+function Content() {
+    return(
+        <div className="main-container">
+            <div>
+                <img className="logo" src={MyImage} alt="Heart" height={150} width={150}></img>
+                <div className="content">
+                <h2>Service with Heart</h2>
+                <p>Make it easy for guests to fell welcomed, inspired and rewarded</p>
+                </div>
+            </div>
+            <div>
+                <form>
+                <div className="form-group">
+                    <label>
+                        <select className="choose">
+                            <option value="email">email</option>
+                            <option value="phone">phone no</option>
+                            <option selected value="user_id">user id</option>
+                            <option value="order_id">order id</option>
+                        </select>
+                    </label>
+                    <input type="text" name="text" className="form-text"/>
+                </div>
+                <input className="button" type="submit" value="Search"/>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default Content
