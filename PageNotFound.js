93ea52dd709a15be462133eb02bf18a2bb396e9b
Jahnavi pabbathi <85212906+Jahnavi-pabbathi@users.noreply.github.com>
2022-08-23 01:23:47 +0530

    Create PageNotFound.js

---
+++ PageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+function PageNotFound(){
+    return(
+        <div>
+            <h1>404 Error</h1>
+            <p> sorry page not found</p>
+        </div>
+    )
+}
+export default PageNotFound
