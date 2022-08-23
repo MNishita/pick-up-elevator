d946298a430fee21b9fa374f6215d263a5d05901
MNishita <mnishita1029@gmail.com>
2022-08-23 08:26:45 +0530

    Created component for Individual Order

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Header from './components/header';
 import Footer from './components/bottombar';
 import Homepage from './pages/main/homepage';
-import Order from './pages/order/order-details';
+import Order from './pages/orders/orders';
+import OrderDetails from  './pages/orderdetails/order-details';
+import Error from './pages/error/404Error';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
@@ -11,9 +14,11 @@ function App() {
       <Header/>
       <Router>
         <Routes>
-          <Route path='/' element={<Homepage/>}/>
+        <Route path='/' element={<Homepage/>}/>
           <Route path='/homepage' element={<Homepage/>}/>
-          <Route path='/orderdetails' element={<Order/>}/>
+          <Route path='/orders' element={<Order/>}/>
+          <Route path='/orders/:orderId' element={<OrderDetails/>}/>
+          <Route path='/orders/404' element={<Error/>}/>
         </Routes>
       </Router>
       <Footer/>
