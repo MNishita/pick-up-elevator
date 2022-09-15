1bf7dc5f69a2f93f468b613c093cd14b32922de8
Varsha4N <varshanippuleti4@gmail.com>
2022-09-16 00:46:19 +0530

    Update Post Request

--- src/pages/QRdetails/Qr.js
+++ src/pages/QRdetails/Qr.js
@@ -4,12 +4,13 @@ import { QRCodeCanvas } from "qrcode.react";
 import { useNavigate,useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getQr } from '../../services/getQr';
+import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
 import { updateDelivery } from "../../services/postDelivery";
 
   function QRgererator() {
 
-    const { mutate} = useMutation(updateDelivery);
+   // const { mutate} = useMutation(updateDelivery);
 
     var showdate = new Date();
     var displaydate =showdate.getFullYear() +'-'+showdate.getMonth()+'-'+showdate.getDate()+'T'+showdate.getHours()+':'+showdate.getMinutes()+':'+showdate.getSeconds()+'+05:30';
@@ -18,8 +19,8 @@ import { updateDelivery } from "../../services/postDelivery";
     let url= '';
     const {orderId,customerId} =useParams();
 
-    const {isLoading, data, isError, error,isFetched } = useQuery(['delivery'], async()=>await getQr({orderId}.orderId,{customerId}.customerId)); 
-
+     const {isLoading, data, isError, error,isFetched } = useQuery(['delivery'], async()=>await getQr({orderId}.orderId,{customerId}.customerId)); 
+    
     if(isFetched) {
       url = data.data.image_code;
     }
@@ -31,6 +32,21 @@ import { updateDelivery } from "../../services/postDelivery";
         bgColor={"white"}
       />
     );
+
+    function dataPost(){
+      axios.post(`http://localhost:8080/aggregator_api/v1/confirm_delivery/order_id/${orderId}/customer_id/${customerId}`,{
+        id : data.data.id,
+        customer_id : data.data.customer_id,
+        store_id : data.data.store_id,
+        order_id : data.data.order_id,
+        image_id : data.data.image_id,
+        image_code : data.data.image_code,
+        payment_status : data.data.payment_status,
+        delivery_status : "DELIVERED",
+        pickup_date : displaydate
+      })
+      .then((response)=>{});
+    }
 
     if (isLoading) return <div>Loading...</div>
 
@@ -45,20 +61,21 @@ import { updateDelivery } from "../../services/postDelivery";
         <p>{qrcode}</p>
         <br></br>
         <h3>Click to confirm delivery</h3>
-        <button className="button3" onClick={() =>{
-          mutate({
-            id : data.data.id,
-            customer_id : data.data.customer_id,
-            store_id : data.data.store_id,
-            order_id : data.data.order_id,
-            image_id : data.data.image_id,
-            image_code : data.data.image_code,
-            payment_status : data.data.payment_status,
-            delivery_status : "DELIVERED",
-            pickup_date : displaydate
+        <button className="button3" onClick={dataPost}
+        // {() =>{
+        //   mutate({
+        //     id : data.data.id,
+        //     customer_id : data.data.customer_id,
+        //     store_id : data.data.store_id,
+        //     order_id : data.data.order_id,
+        //     image_id : data.data.image_id,
+        //     image_code : data.data.image_code,
+        //     payment_status : data.data.payment_status,
+        //     delivery_status : "DELIVERED",
+        //     pickup_date : displaydate
             
-          })
-        }}
+        //   })
+        // }}
         >
           Confirm Delivery
         </button>
